f8a5f90eea29096278e4384e8db3bb27c6d11558
Slisandro <salvareschilisandro0@gmail.com>
2025-02-23 22:38:22 -0300

    create navbar component in module/navbar

---
+++ navbar/src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Auth from "./Auth";
+import Link from "./Link";
+import ListBoards from "./ListBoards";
+
+const Navbar = () => {
+    return (
+        <div className="w-64 h-screen bg-gray-900 text-white fixed flex flex-col justify-between">
+            <div>
+                <div className="p-4 text-2xl font-bold border-b border-gray-700 cursor-default">
+                    Project Manager
+                </div>
+                <ul className="mt-4">
+                    <li
+                        className="p-4 hover:bg-gray-800 cursor-pointer"
+                    >
+                        <Link to="/home" content="🏠 Inicio" />
+                    </li>
+                    <ListBoards />
+                </ul>
+            </div>
+            <ul className="mb-4">
+                <li
+                    className="p-4 hover:bg-gray-800 cursor-pointer"
+                >
+                    <Link to="/configuration" content="⚙️ Configuración" />
+                </li>
+                <Auth />
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
