d50efeae8d5be2ee6838ffb1e033df9d879701cc
Lisandro Salvareschi <salvareschilisandro0@gmail.com>
2025-03-13 17:08:55 +0000

    change navbar component

--- navbar/src/components/Navbar.tsx
+++ navbar/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import ListBoards from "./ListBoards";
 
 const Navbar = () => {
     return (
-        <div className="w-64 h-screen bg-gray-900 text-white fixed flex flex-col justify-between">
+        <div className="w-64 h-screen bg-[#111827] text-white fixed flex flex-col justify-between">
             <div>
                 <div className="p-4 text-2xl font-bold border-b border-gray-700 cursor-default">
                     Project Manager
