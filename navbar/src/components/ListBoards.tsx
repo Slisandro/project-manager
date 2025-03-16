b77bdfbc015a2d9ba7916bc7784c6d943e91298b
Lisandro Salvareschi <salvareschilisandro0@gmail.com>
2025-03-16 23:45:09 +0000

    hardcode boards

--- navbar/src/components/ListBoards.tsx
+++ navbar/src/components/ListBoards.tsx
@@ -18,22 +18,12 @@ export default function ListBoards() {
       <li className="p-4 text-gray-400 uppercase text-sm cursor-default">
         📋 &nbsp; Tableros
       </li>
-      {/* {true ? ( */}
-      <>
-        {boards.map((board) => (
-          <li
-            key={board.id}
-            className="pl-6 pr-4 py-2 hover:bg-gray-800 cursor-pointer"
-          >
-            <Link to={"/b/" + board.id} content={board.name} />
-          </li>
-        ))}
-      </>
-      {/* ) : (
-        <li className="opacity-25 pl-6 pr-4 py-2 cursor-default">
-          No hay tableros aún
-        </li>
-      )} */}
+      <li className="pl-6 pr-4 py-2 hover:bg-gray-800 cursor-pointer">
+        <Link to={"/b/" + boards[0].id} content={boards[0].name} />
+      </li>
+      <li className="pl-6 pr-4 py-2 hover:bg-gray-800 cursor-pointer">
+        <Link to={"/b/" + boards[1].id} content={boards[1].name} />
+      </li>
     </>
   );
 }
