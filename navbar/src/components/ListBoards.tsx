cef8f2a49dd6e556600333b66decb79ed890ef5f
Lisandro Salvareschi <salvareschilisandro0@gmail.com>
2025-03-16 23:26:57 +0000

    fix boards

--- navbar/src/components/ListBoards.tsx
+++ navbar/src/components/ListBoards.tsx
@@ -1,26 +1,27 @@
 import Link from "./Link";
 
-
 export default function ListBoards() {
   const boards: { id: number; name: string; description: string }[] | [] = [
     { id: 1, name: "Proyecto A", description: "Planificación inicial" },
     { id: 2, name: "Proyecto B", description: "Marketing y diseño" },
   ];
-  
+
   return (
     <>
       <li className="p-4 text-gray-400 uppercase text-sm cursor-default">
         📋 &nbsp; Tableros
       </li>
       {true ? (
-        boards.map((board) => (
-          <li
-            key={board.id}
-            className="pl-6 pr-4 py-2 hover:bg-gray-800 cursor-pointer"
-          >
-            <Link to={"/b/" + board.id} content={board.name} />
-          </li>
-        ))
+        <>
+          {boards.map((board) => (
+            <li
+              key={board.id}
+              className="pl-6 pr-4 py-2 hover:bg-gray-800 cursor-pointer"
+            >
+              <Link to={"/b/" + board.id} content={board.name} />
+            </li>
+          ))}
+        </>
       ) : (
         <li className="opacity-25 pl-6 pr-4 py-2 cursor-default">
           No hay tableros aún
