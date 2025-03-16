28e5d2408e41c372eec08ddc7acade5f5a546e98
Lisandro Salvareschi <salvareschilisandro0@gmail.com>
2025-03-16 03:55:04 +0000

    design sidebar

--- navbar/src/components/Auth.tsx
+++ navbar/src/components/Auth.tsx
@@ -3,18 +3,18 @@ import { NavLink } from "react-router-dom";
 import Link from "./Link";
 
 const AuthButton = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    return (
-        <li
-            className="p-4 hover:bg-gray-800 cursor-pointer"
-        >
-            <Link 
-                to={isLoggedIn ? "/logout" : "/login"} 
-                content={`🚪 ${isLoggedIn ? "Cerrar sesión" : "Iniciar sesión"}`} 
-            />
-        </li>
-    );
+  return (
+    <li className="p-4 hover:bg-gray-800 cursor-pointer">
+      <Link
+        to={isLoggedIn ? "/logout" : "/login"}
+        content={
+          "🚪  " + `🚪 ${isLoggedIn ? "Cerrar sesión" : "Iniciar sesión"}`
+        }
+      />
+    </li>
+  );
 };
 
 export default AuthButton;
--- navbar/src/components/ListBoards.tsx
+++ navbar/src/components/ListBoards.tsx
@@ -1,24 +1,30 @@
-import { NavLink } from "react-router-dom";
 import Link from "./Link";
 
-const boards: { id: number, name: string }[] | [] = [
-    // { id: 1, name: "Proyecto A", description: "Planificación inicial" },
-    // { id: 2, name: "Proyecto B", description: "Marketing y diseño" },
-]
+const boards: { id: number; name: string; description: string }[] | [] = [
+  { id: 1, name: "Proyecto A", description: "Planificación inicial" },
+  { id: 2, name: "Proyecto B", description: "Marketing y diseño" },
+];
 
 export default function ListBoards() {
-    return (
-        <>
-            <li className="p-4 text-gray-400 uppercase text-sm cursor-default">📋 Tableros</li>
-            {boards.length ? boards.map((board) => (
-                <li
-                    key={board.id}
-                    className="pl-6 pr-4 py-2 hover:bg-gray-800 cursor-pointer"
-                >
-                    <Link to={"/boards/" + board.id} content={board.name} />
-                </li>
-            )) : <li className="opacity-25 pl-6 pr-4 py-2 cursor-default">No hay tableros aún</li>
-            }
-        </>
-    )
+  return (
+    <>
+      <li className="p-4 text-gray-400 uppercase text-sm cursor-default">
+        📋 &nbsp; Tableros
+      </li>
+      {boards.length ? (
+        boards.map((board) => (
+          <li
+            key={board.id}
+            className="pl-6 pr-4 py-2 hover:bg-gray-800 cursor-pointer"
+          >
+            <Link to={"/boards/" + board.id} content={board.name} />
+          </li>
+        ))
+      ) : (
+        <li className="opacity-25 pl-6 pr-4 py-2 cursor-default">
+          No hay tableros aún
+        </li>
+      )}
+    </>
+  );
 }
