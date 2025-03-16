289f21245940dddae7156f693ce96648d7ba486d
Lisandro Salvareschi <salvareschilisandro0@gmail.com>
2025-03-16 21:17:56 +0000

    add .env container app

--- container/module-federation.config.ts
+++ container/module-federation.config.ts
@@ -2,8 +2,8 @@ export const mfConfig = {
   name: "container",
   exposes: {},
   remotes: {
-    navbar: "navbar@https://szhp6l-3001.csb.app/navbar.js",
-    content: "content@https://szhp6l-3002.csb.app/content.js",
+    navbar: "navbar@" + process.env.REACT_APP_NAVBAR + "navbar.js",
+    content: "content@" + process.env.REACT_APP_CONTENT + "content.js",
   },
   shared: ["react", "react-dom", "tailwindcss", "react-router-dom"],
 };
